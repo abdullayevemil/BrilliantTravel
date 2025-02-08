5be39d96d28491ed3e60e9cc92e587dfc3a13e8a
Emil Abdullayev <abdullayevemil27042006@gmail.com>
2025-02-08 22:37:30 +0400

    Add header

--- brilliant-travel/src/components/layout/header.tsx
+++ brilliant-travel/src/components/layout/header.tsx
@@ -1,9 +1,28 @@
-
+import Logo from "@/assets/images/brilliant-travel-logo.svg";
+import Link from "next/link";
 
 export default function Header() {
-    return (
-        <header>
+  return (
+    <header className="px-24 py-[18] flex flex-row items-center justify-between">
+      <Logo />
 
-        </header>
-    );
+      <nav>
+        <ul className="flex flex-row gap-4">
+          <li className="px-2 py-1.5">
+            <Link href="/destinations" className="text-navigation">Destinations</Link>
+          </li>
+
+          <li className="px-2 py-1.5">
+            <Link href="/gallery" className="text-navigation">Gallery</Link>
+          </li>
+
+          <li className="px-2 py-1.5">
+            <Link href="/about" className="text-navigation">About</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Link href="/contact" className="px-4 py-2 border-border border rounded-full">Contact us</Link>
+    </header>
+  );
 }
--- brilliant-travel/tailwind.config.ts
+++ brilliant-travel/tailwind.config.ts
@@ -11,6 +11,8 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        navigation: "var(--navigation-foreground)",
+        border: "var(--navigation-border)",
       },
     },
   },
