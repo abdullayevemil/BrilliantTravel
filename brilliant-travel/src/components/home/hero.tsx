f1f618eaef5dd756dd6b8f3d01aa2c3c732055c0
Emil Abdullayev <abdullayevemil27042006@gmail.com>
2025-02-13 23:02:29 +0400

    Add heor image width

--- brilliant-travel/src/components/home/hero.tsx
+++ brilliant-travel/src/components/home/hero.tsx
@@ -27,7 +27,7 @@ const AnimationLottie = () => {
 export default function Hero() {
   return (
     <div className="relative">
-      <Image src={HeroImage} alt="Hero image" className="" />
+      <Image src={HeroImage} alt="Hero image" className="w-full" />
 
       <div className="absolute left-0 bottom-0 w-full h-[8.3125rem] bg-gradient z-50 flex justify-center items-center">
         <div className="py-2 px-3 rounded-full flex flex-row gap-2 border border-border">
