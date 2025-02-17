95fda7e03c2b75a361a847368d6d76b478cbf3e5
Emil Abdullayev <abdullayevemil27042006@gmail.com>
2025-02-17 20:18:54 +0400

    Update maiden tower part

--- brilliant-travel/src/app/page.tsx
+++ brilliant-travel/src/app/page.tsx
@@ -1,7 +1,7 @@
 import Benefits from "@/components/home/benefits";
 import Hero from "@/components/home/hero";
 import Image from "next/image";
-import QizQalasi from '@/assets/images/qiz-qalasi copy.png';
+import QizQalasi from "@/assets/images/qiz-qalasi copy.png";
 
 export default function Home() {
   return (
@@ -10,14 +10,14 @@ export default function Home() {
         <Hero />
       </section>
 
-      <section className="px-24 w-full flex flex-col gap-7">
+      <section className="px-24 w-full flex flex-col gap-7 mb-16">
         <Benefits />
       </section>
 
       <section className="px-24 w-full flex flex-col gap-7">
-        <div className="w-full bg-primary rounded-2xl w-full p-[2.5rem] flex flex-row justify-between relative">
+        <div className="w-full bg-primary rounded-2xl w-full p-[2.5rem] flex flex-row justify-between relative h-[22.5rem]">
           <div className="flex flex-col gap-7">
-            <div className="flex flex-col gap-3">
+            <div className="flex flex-col h-full justify-between">
               <h2 className="font-semibold text-[2.625rem]">Who we are?</h2>
 
               <p className="w-1/2">
@@ -27,12 +27,18 @@ export default function Home() {
                 effortless. Our dedicated team ensures every journey is
                 unforgettable. Let’s make your next trip brilliant!
               </p>
+              
+              <button className="w-fit px-6 py-3 rounded-full border border-border">
+                Learn more
+              </button>
             </div>
-
-            <button className="w-fit px-6 py-3 rounded-full border border-border">Learn more</button>
           </div>
 
-          <Image className="absolute bottom-0 right-0" src={QizQalasi} alt="qiz qalasi" />
+          <Image
+            className="absolute bottom-0 right-0 h-[26.5rem]"
+            src={QizQalasi}
+            alt="qiz qalasi"
+          />
         </div>
       </section>
     </main>
